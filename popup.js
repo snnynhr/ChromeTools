b2d1fb811cef640292317bfecf86fa37d05cdb3e
Sunny Nahar <sunnyn@cmu.edu>
2014-06-18 23:59:27 -0400

    Added cookies setting

--- popup.js
+++ popup.js
@@ -1,4 +1,4 @@
-function toggle()
+function togglePopups()
 {
 	if(document.getElementById("enabled").classList.contains("off"))
 	{
@@ -17,6 +17,25 @@ function toggle()
 		document.getElementById("enabled").classList.toggle("off");
 	}
 }
+function toggleCookies()
+{
+	if(document.getElementById("cookies").classList.contains("off"))
+	{
+		chrome.contentSettings['cookies'].set({
+		        'primaryPattern': '<all_urls>',
+		        'setting': 'block'
+		      });
+		document.getElementById("cookies").classList.toggle("off");
+	}
+	else
+	{
+		chrome.contentSettings['cookies'].set({
+		        'primaryPattern': '<all_urls>',
+		        'setting': 'allow'
+		      });
+		document.getElementById("cookies").classList.toggle("off");
+	}
+}
 function clearCookies()
 {
 	document.getElementById("clickhide").innerHTML = '<progress id = "pbar" value="0" max="100"></progress>';
@@ -29,6 +48,7 @@ function clearCookies()
 }
 function init()
 {
+	/* Initialize settings */
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 	    var current = tabs[0];
 	    incognito = current.incognito;
@@ -36,23 +56,30 @@ function init()
       	chrome.contentSettings.popups.get({
             'primaryUrl': url,
             'incognito': incognito
-          },
-          function(details) {
-            if(details.setting=="allow")
-            {
- 				document.getElementById("enabled").classList.add("off");           	
-            }
-            else
-            {
-            	if(document.getElementById("enabled").classList.contains("off"))
+        	},
+          	function(details) {
+            	if(details.setting=="allow")
+ 					document.getElementById("enabled").classList.add("off");           	
+            	else if(document.getElementById("enabled").classList.contains("off"))
             		document.getElementById("enabled").classList.toggle("off");
-            }
-          });});
-	document.getElementById("enabled").addEventListener("click",toggle);
+        });
+        chrome.contentSettings.cookies.get({
+            'primaryUrl': url,
+            'incognito': incognito
+        	},
+          	function(details) {
+            	if(details.setting=="allow" || details.setting=="session_only")
+ 					document.getElementById("cookies").classList.add("off");           	
+            	else if(document.getElementById("cookies").classList.contains("off"))
+            		document.getElementById("cookies").classList.toggle("off");
+        });
+
+    });
+
+	document.getElementById("enabled").addEventListener("click",togglePopups);
+	document.getElementById("cookies").addEventListener("click",toggleCookies);
 	document.getElementById("clickhide").addEventListener('click',clearCookies);
 	document.getElementById("options").addEventListener("click", function()
-	{
-		openOptions();
-	}, false);
+	{});
 }
 window.addEventListener("DOMContentLoaded", init, false);
