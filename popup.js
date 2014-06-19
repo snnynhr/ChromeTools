e98ac14f0f175f022a9a499c22e83b64c9bb4168
Sunny Nahar <sunnyn@cmu.edu>
2014-06-18 23:32:40 -0400

    Modified event handlers + popups option

--- popup.js
+++ popup.js
@@ -1,30 +1,58 @@
 function toggle()
 {
-	var toggle = document.getElementById("popup_status").innerHTML;
-	if(toggle == "ON")
+	if(document.getElementById("enabled").classList.contains("off"))
 	{
-		document.getElementById("popup_status").innerHTML = "OFF";
-		document.getElementById("popup_status").style.cssText = "color:red;"
+		chrome.contentSettings['popups'].set({
+		        'primaryPattern': '<all_urls>',
+		        'setting': 'block'
+		      });
+		document.getElementById("enabled").classList.toggle("off");
 	}
 	else
 	{
-		document.getElementById("popup_status").innerHTML = "ON";
-		document.getElementById("popup_status").style.cssText = "color:green;"
+		chrome.contentSettings['popups'].set({
+		        'primaryPattern': '<all_urls>',
+		        'setting': 'allow'
+		      });
+		document.getElementById("enabled").classList.toggle("off");
 	}
 }
 function clearCookies()
 {
-	document.getElementById("cookies").innerHTML = '<progress id = "pbar" value="0" max="100"></progress>';
+	document.getElementById("clickhide").innerHTML = '<progress id = "pbar" value="0" max="100"></progress>';
 	var i;
 	for(i=1; i<=100; i++)
 	{
 		document.getElementById("pbar").value = i + "";
 	}
-	document.getElementById("cookies").innerHTML = "Clear Cookies";
+	document.getElementById("clickhide").innerHTML = '<div class="icon"></div><span> Clear Cookies</span>';
 }
 function init()
 {
-	document.getElementById("popup").addEventListener('click',toggle);
-	document.getElementById("cookies").addEventListener('click',clearCookies);
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+	    var current = tabs[0];
+	    incognito = current.incognito;
+	    url = current.url;
+      	chrome.contentSettings.popups.get({
+            'primaryUrl': url,
+            'incognito': incognito
+          },
+          function(details) {
+            if(details.setting=="allow")
+            {
+ 				document.getElementById("enabled").classList.add("off");           	
+            }
+            else
+            {
+            	if(document.getElementById("enabled").classList.contains("off"))
+            		document.getElementById("enabled").classList.toggle("off");
+            }
+          });});
+	document.getElementById("enabled").addEventListener("click",toggle);
+	document.getElementById("clickhide").addEventListener('click',clearCookies);
+	document.getElementById("options").addEventListener("click", function()
+	{
+		openOptions();
+	}, false);
 }
 window.addEventListener("DOMContentLoaded", init, false);
