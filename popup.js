b424aace5a0e8e76464b272f05d9380eba3396c9
Sunny Nahar <sunnyn@cmu.edu>
2014-06-18 21:59:29 -0400

    Refactored UI page

--- popup.js
+++ popup.js
@@ -12,8 +12,19 @@ function toggle()
 		document.getElementById("popup_status").style.cssText = "color:green;"
 	}
 }
+function clearCookies()
+{
+	document.getElementById("cookies").innerHTML = '<progress id = "pbar" value="0" max="100"></progress>';
+	var i;
+	for(i=1; i<=100; i++)
+	{
+		document.getElementById("pbar").value = i + "";
+	}
+	document.getElementById("cookies").innerHTML = "Clear Cookies";
+}
 function init()
 {
 	document.getElementById("popup").addEventListener('click',toggle);
+	document.getElementById("cookies").addEventListener('click',clearCookies);
 }
 window.addEventListener("DOMContentLoaded", init, false);
