577745d6545484ed27b594eb3c5bf891ed04ad7f
Sunny Nahar <sunnyn@cmu.edu>
2014-07-03 18:43:10 -0400

    Added History/Cookies Whitelist UI in Options page

--- options.js
+++ options.js
@@ -42,23 +42,86 @@ function updateSize()
 }
 function loadHistoryWL()
 {
-
-}
-function loadCookieWL()
-{
+	var res = "";
+	var arr = JSON.parse(localStorage.getItem("historyWL"));
+	for(var i = arr.length-1; i>=0; i--)
+	{
+		res += "<option value=" + i.toString() +  ">"+arr[i]+"</option>\n";
+	}
+	document.getElementById("history").innerHTML = res;
 	
+}
+function loadCookiesWL()
+{
+	var res = "";
+	var arr = JSON.parse(localStorage.getItem("cookiesWL"));
+	for(var i = arr.length-1; i>=0; i--)
+	{
+		res += "<option value=" + i.toString() +  ">"+arr[i]+"</option>\n";
+	}
+	document.getElementById("cookies").innerHTML = res;
+}
+function addToHistoryWL()
+{
+	var elem = document.getElementById('addhistinput').value;
+	var arr = JSON.parse(localStorage.getItem("historyWL"));
+	arr[arr.length] = elem;
+	localStorage.setItem("historyWL",JSON.stringify(arr));
+	console.log(arr);
+	loadHistoryWL();
+}
+function addToCookiesWL()
+{
+	var elem = document.getElementById('addcookiesinput').value;
+	var arr = JSON.parse(localStorage.getItem("cookiesWL"));
+	arr[arr.length] = elem;
+	localStorage.setItem("cookiesWL",JSON.stringify(arr));
+	loadCookiesWL();
+}
+function deleteHistoryEntries()
+{
+	var arr = document.getElementById('history').options;
+	var res = [];
+	for(var i = 0; i<arr.length; i++)
+	{
+		var opt = arr[arr.length-1-i];
+		if(!opt.selected)
+		{
+			res[res.length] = opt.text;
+		}
+	}
+	localStorage.setItem("historyWL",JSON.stringify(res));
+	loadHistoryWL();
+}
+function deleteCookiesEntries()
+{
+	var arr = document.getElementById('cookies').options;
+	var res = [];
+	for(var i = 0; i<arr.length; i++)
+	{
+		var opt = arr[arr.length-1-i];
+		if(!opt.selected)
+		{
+			res[res.length] = opt.text;
+		}
+	}
+	localStorage.setItem("cookiesWL",JSON.stringify(res));
+	loadCookiesWL();
 }
 function exec() {
 	loadQuality();
 	loadSize();
 	loadHistoryWL();
-	loadCookieWL();
+	loadCookiesWL();
 	document.getElementById('ytQ1').addEventListener('click',updateQuality);
 	document.getElementById('ytQ2').addEventListener('click',updateQuality);
 	document.getElementById('ytQ3').addEventListener('click',updateQuality);
 	document.getElementById('ytQ4').addEventListener('click',updateQuality);
 	document.getElementById('ytS1').addEventListener('click',updateSize);
 	document.getElementById('ytS2').addEventListener('click',updateSize);
-
+	document.getElementById('addhist').addEventListener('click',addToHistoryWL);
+	document.getElementById('addcookies').addEventListener('click',addToCookiesWL);
+	document.getElementById('delhist').addEventListener('click',deleteHistoryEntries);
+	document.getElementById('delcookies').addEventListener('click',deleteCookiesEntries);
 }
 document.addEventListener('DOMContentLoaded', exec);
