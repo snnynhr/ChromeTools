062593a2f74e8cd2a0004e529beca80e15d1e3c5
Sunny Nahar <sunnyn@cmu.edu>
2014-07-03 17:28:37 -0400

    Modified Options UI, added frames

--- options.js
+++ options.js
@@ -1,19 +1,15 @@
-// Saves options to chrome.storage
-function save_options() {
-  localStorage.setItem("dss", document.getElementById('dss').value);
-  localStorage.setItem("min", document.getElementById('min').value);
-  chrome.runtime.sendMessage({greeting: "updated"}, function(response) {
-  console.log(response.farewell);
-	});
+function loadQuality()
+{
+	var n = localStorage.getItem("ytQuality");
+	if(n == "highres")
 }
 
-// Restores select box and checkbox state using the preferences
-// stored in chrome.storage.
-function restore_options() {
-  document.getElementById('dss').value = localStorage.getItem("dss");
-  document.getElementById('min').value = localStorage.getItem("min");
-}
 
-document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('save').addEventListener('click',
+
+
+function exec() {
+	loadQuality();
+	document.getElementById('save').addEventListener('click',
     save_options);
+}
+document.addEventListener('DOMContentLoaded', exec);
